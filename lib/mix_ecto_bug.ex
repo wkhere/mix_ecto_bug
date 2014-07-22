d16a8d9d2ecf5857fb6bc1ad4c7062eb3462e678
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-22 21:38:10 +0200

    still ok

--- lib/mix_ecto_bug.ex
+++ lib/mix_ecto_bug.ex
@@ -9,6 +9,7 @@ defmodule MixEctoBug do
     children = [
       # Define workers and child supervisors to be supervised
       # worker(MixEctoBug.Worker, [arg1, arg2, arg3])
+      worker(Repo, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
