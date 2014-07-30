310a1d656a520ac847a2dbd3b6cc3b9a964ef242
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-30 18:18:34 +0200

    still ok

--- lib/model/init.ex
+++ lib/model/init.ex
@@ -1,5 +1,5 @@
 defmodule Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres, env: Mix.env
 
-  def conf(_env), do: parse_url "not_important"
+  def conf(_env), do: parse_url "ecto://localhost/postgres"
 end
