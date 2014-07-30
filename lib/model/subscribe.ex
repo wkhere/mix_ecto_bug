8d6a4fa9ae0b64c24ef41cd90c3a2b355368dfb2
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-07-30 18:39:41 +0200

    mark offending code

--- lib/model/subscribe.ex
+++ lib/model/subscribe.ex
@@ -1,17 +1,7 @@
 defmodule Subscribe do
-
-  @spec days_from_payment(Payment.t) :: pos_integer
-  def days_from_payment(p) do
-    if p.currency == "PLN" do
-          cond do
-        p.amount >= 10 && p.amount < 27   -> 31
-        p.amount >= 27 && p.amount < 54   -> 92
-        p.amount >= 54 && p.amount < 108  -> 184
-        p.amount >= 108                   -> 366
-      end
-    end
-  end
-
+  # OFFENDING:
   require Payment
-  #def foo(%Payment{giver_address: addr}), do: :ok
+
+  # also OFFENDING:
+  def foo(%Payment{giver_address: addr}), do: :ok
 end
